e9f39f83576f33396759d8829a257bbd87db321a
Shubham diyundi <shubhamdundhy@gmail.com>
2024-01-23 21:29:42 +0530

    Update untoldcoding.js

--- untoldcoding.js
+++ untoldcoding.js
@@ -2,7 +2,7 @@ var i = 0;
 
 var txt1 =
   " Hi Cutie.....!  <<               Now I wan't to say something special to you. <<<                So , Please read everything carefully...!                                                                           > When I saw you for the first time..! <You appear to be something special, and my inner spirit encourages me to speak with you..  <<                                      > Everything about you is so special...!               << I am somewhat nervous Because I haven't said these words to anyone and I won't say to anyone in future...!                                                     > Beyond everything else in this world, I love my parents...!                      <<  You are the only person I love just as much as my parents right now....!";
-var speed = 50;
+var speed = 65;
 typeWriter();
 function typeWriter() {
   if (i < txt1.length) {
@@ -17,7 +17,13 @@ function typeWriter() {
     setTimeout(typeWriter, speed);
   }
 }
+// function playAudio() {
+//   var audio = document.getElementById("myAudio");
+//   audio.play();
+//   document.getElementById("delayedButton").style.display = "none";
+// }
 
-var audio = new Audio("music/firstsong.mp3");
-audio.loop = true;
-audio.play();
+// // Set a timer to automatically trigger the button click after 10 seconds
+// setTimeout(function () {
+//   playAudio();
+// }, 1000);
